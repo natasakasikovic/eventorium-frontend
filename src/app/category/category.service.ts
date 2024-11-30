262a0457a902db74d810db7d072802ccdf27be53
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-11-30 15:34:56 +0100

    chore: Move apiHost into env folder

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -3,6 +3,7 @@ import {Category} from './model/category.model';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {CategoryRequestDto} from './model/category-request-dto.model';
+import {environment} from '../../env/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,26 +16,26 @@ export class CategoryService {
   }
 
   create(category: CategoryRequestDto): Observable<Category> {
-    return this.httpClient.post<Category>("http://localhost:8080/api/v1/categories", category);
+    return this.httpClient.post<Category>(`${environment.apiHost}/categories`, category);
   }
 
   getAll(): Observable<Category[]> {
-    return this.httpClient.get<Category[]>("http://localhost:8080/api/v1/categories/all");
+    return this.httpClient.get<Category[]>(`${environment.apiHost}/categories/all`);
   }
 
   get(id: number): Observable<Category> {
-    return this.httpClient.get<Category>(`http://localhost:8080/api/v1/categories/${id}`);
+    return this.httpClient.get<Category>(`${environment.apiHost}/categories/${id}`);
   }
 
   update(id: number, category: CategoryRequestDto): Observable<Category> {
-    return this.httpClient.put<Category>(`http://localhost:8080/api/v1/categories/${id}`, category);
+    return this.httpClient.put<Category>(`${environment.apiHost}/categories/${id}`, category);
   }
 
   delete(id: number): Observable<void> {
-    return this.httpClient.delete<void>(`http://localhost:8080/api/v1/categories/${id}`);
+    return this.httpClient.delete<void>(`${environment.apiHost}/categories/${id}`);
   }
 
   getAllProposals(): Observable<Category[]> {
-    return this.httpClient.get<Category[]>("http://localhost:8080/api/v1/categories/pending/all");
+    return this.httpClient.get<Category[]>(`${environment.apiHost}/categories/pending/all`);
   }
 }
