dd2ef84dd1624f79a67acec6a858a8505f6961fb
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-12-23 01:46:51 +0100

    feat: implement error handling

--- src/app/auth/quick-registration/quick-registration.component.ts
+++ src/app/auth/quick-registration/quick-registration.component.ts
@@ -4,12 +4,14 @@ import { QuickRegistrationDto } from '../model/quick-registration.model';
 import { AuthService } from '../auth.service';
 import { QuickRegistrationPersonDto } from '../model/qucik-registration-person.model';
 import { MatDialog } from '@angular/material/dialog';
-import { catchError, switchMap, tap } from 'rxjs';
+import { catchError, of, switchMap, tap } from 'rxjs';
 import { InfoDialogComponent } from '../../shared/info-dialog/info-dialog.component';
 import { MESSAGES } from '../../shared/constants/messages';
-import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { EventService } from '../../event/event.service';
 import { InvitationResponse } from '../../event/model/invitation-response.model';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ERROR_MESSAGES } from '../../shared/constants/error-messages';
 
 @Component({
   selector: 'app-quick-registration',
@@ -46,10 +48,12 @@ export class QuickRegistrationComponent implements OnInit {
     ).subscribe({
       next: (response: InvitationResponse) => {
         this.email = response.email;
+      },
+      error: (_) => {
+      this.showMessage(ERROR_MESSAGES.GENERAL_ERROR, ERROR_MESSAGES.SERVER_ERROR)
       }
     });
   }
-
 
   passwordMatchValidator(group: FormGroup): { [key: string]: boolean } | null {
     const password = group.get('password').value;
@@ -64,13 +68,21 @@ export class QuickRegistrationComponent implements OnInit {
     const user = this.getFormValues()
 
     this.service.quickRegister(user).pipe(
-      
       tap(() => {
         const message = MESSAGES.quickRegistrationSuccess;
         this.showMessage(message.title, message.message)
         this.router.navigate(['/'])
+      }),
+      catchError((error: HttpErrorResponse) => {
+        if (error.status == 409) {
+          this.showMessage(ERROR_MESSAGES.GENERAL_ERROR, ERROR_MESSAGES.EMAIL_TAKEN_QUICK_REGISTRATION)
+        } else if (error.status == 400) {
+          this.showMessage(ERROR_MESSAGES.GENERAL_ERROR, ERROR_MESSAGES.INPUT_VALIDATION_ERROR)
+        } else {
+          this.showMessage(ERROR_MESSAGES.GENERAL_ERROR, ERROR_MESSAGES.SERVER_ERROR);
+        }
+        return of(null);
       })
-      // TODO: add catchError
     ).subscribe();
   }
 
