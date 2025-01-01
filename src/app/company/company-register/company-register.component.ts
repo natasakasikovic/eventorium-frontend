f289ca74cb1b54e92caf77c857106c8643eac812
Ana Šinik <sinikana7@gmail.com>
2025-01-01 16:43:58 +0100

    feat: implement company registration

--- src/app/company/company-register/company-register.component.ts
+++ src/app/company/company-register/company-register.component.ts
@@ -1,51 +1,87 @@
-import { Component } from '@angular/core';
-import { CompanyModule } from '../company.module';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Company } from '../model/company.model';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CompanyService } from '../company.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { City } from '../../shared/model/city.model';
+import { SharedService } from '../../shared/shared.service';
+import { MatDialog } from '@angular/material/dialog';
+import { InfoDialogComponent } from '../../shared/info-dialog/info-dialog.component';
+import { MESSAGES } from '../../shared/constants/messages';
+import { CompanyRequest } from '../model/company-request.model';
+import { CompanyResponse } from '../model/company-response.model';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ERROR_MESSAGES } from '../../shared/constants/error-messages';
 
 @Component({
   selector: 'app-company-register',
   templateUrl: './company-register.component.html',
   styleUrl: './company-register.component.css'
 })
-export class CompanyRegisterComponent {
+export class CompanyRegisterComponent implements OnInit {
   images: string[] = []; 
   companyForm: FormGroup;
+  cities: City[];
+  providerId: number;
 
-  constructor(private fb: FormBuilder, private companyService: CompanyService, private router: Router) {
+  constructor(private fb: FormBuilder, 
+              private companyService: CompanyService,   
+              private router: Router,
+              private sharedService: SharedService,
+              private route: ActivatedRoute,
+              private dialog: MatDialog) {
     this.companyForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       name: ['', Validators.required],
       address: ['', Validators.required],
       city: ['', Validators.required],
-      phone_number: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
-      description: ['', Validators.required]
+      phoneNumber: ['', [Validators.required, Validators.pattern('^\\+?[0-9]{10,15}$')]],
+      description: ['', Validators.required],
+      openingHours: ['', Validators.required],
+      closingHours: ['', Validators.required]
+    });
+
+    this.getCities();
+  }
+
+  ngOnInit(): void {
+    this.loadParams();
+    console.log('Provider ID:', this.providerId);
+  }
+
+  private loadParams(): void {
+    this.route.params.subscribe(params => {
+      this.providerId = params['provider-id'] ?? null;
     });
   }
 
   onSubmit() {
     if (this.companyForm.valid) {
-      const newCompany: Company = this.companyForm.value;
-      // this.companyService.add(newCompany); // NOTE
-      
-      // NOTE: This will be changed to avoid using 'alert' in the future.
-      alert(`Account Activation Required
-          Thank you for signing up!
-          
-          To complete your registration, please follow these steps:
-          
-          1. Check your email: We've sent an activation link to the email address you provided.
-          2. Activate your account: Click the link within 24 hours to confirm your email and activate your account.
-          
-          Important: Your activation link will expire after 24 hours. If you do not activate your account within this time, you will need to repeat the registration process.
-          
-          Thank you for choosing Eventorium!`);
-          
-      this.router.navigate(['/']);
-    
+      const newCompany: CompanyRequest = this.companyForm.value;
+      newCompany.providerId = this.providerId;
+      console.log(JSON.stringify(newCompany));
+      this.companyService.createCompany(newCompany).subscribe({
+        next: (response: CompanyResponse) => {       
+          this.showMessage(MESSAGES.accountActivation.title, MESSAGES.accountActivation.message);
+          this.router.navigate(['/']);
+        },
+        error: (error: HttpErrorResponse) => {
+          if (error.status == 400) {
+            this.showMessage(ERROR_MESSAGES.GENERAL_ERROR, error.message);
+          } else {
+            this.showMessage(ERROR_MESSAGES.GENERAL_ERROR, ERROR_MESSAGES.SERVER_ERROR);
+          }
+        }
+      })
     }
+  }
+
+  showMessage(title: string, message: string) : void {
+    this.dialog.open(InfoDialogComponent, {
+      data: {
+        title: title,
+        message: message
+      }
+    })
   }
 
   get formControls() {
@@ -70,5 +106,9 @@ export class CompanyRegisterComponent {
 
   deleteImage(index: number): void {
     this.images.splice(index, 1); 
+  }
+
+  getCities(): void {
+    this.sharedService.getCities().subscribe(cities => this.cities = cities);
   }
 }
--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../env/environment';
+import { CompanyRequest } from './model/company-request.model';
+import { CompanyResponse } from './model/company-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createCompany(company: CompanyRequest): Observable<CompanyResponse> {
+    return this.http.post<CompanyResponse>(`${environment.apiHost}/companies`, company)
+  }
 }
