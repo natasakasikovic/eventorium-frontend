62b0881f9b70876ced74bae1f186c471663f9154
Lazar Nagulov <lazarnagulov1@gmail.com>
2025-06-09 16:49:45 +0200

    refactor: Models to fit new API

--- src/app/review/model/comment.model.ts
+++ src/app/review/model/comment.model.ts
@@ -1,6 +1,5 @@
 import {UserDetails} from '../../user/model/user-details.model';
 import {ReviewType} from './review-type.enum';
-import {Commentable} from './commentable.model';
 
 export interface Comment {
   id: number;
@@ -8,5 +7,6 @@ export interface Comment {
   creationDate: Date;
   user: UserDetails;
   type: ReviewType;
-  commentable: Commentable;
+  objectId: number;
+  displayName: string;
 }
--- src/app/review/model/commentable.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Commentable {
-  id: number;
-  displayName: string;
-}
